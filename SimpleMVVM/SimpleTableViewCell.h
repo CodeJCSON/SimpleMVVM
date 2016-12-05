f9e7cab1701a2f0a216010aede2f0aa3353d3fd5
guowilling <guowilling90@gmail.com>
2016-12-05 21:16:29 +0800

    First commit

---
+++ SimpleMVVM/SimpleTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  SimpleTableViewCell.h
+//  SimpleMVVM
+//
+//  Created by 郭伟林 on 16/12/5.
+//  Copyright © 2016年 SR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SimpleModel.h"
+
+@interface SimpleTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) SimpleModel *simpleModel;
+
+@end
---
+++ SimpleMVVM/SimpleTableViewCell.m
@@ -0,0 +1,32 @@
+//
+//  SimpleTableViewCell.m
+//  SimpleMVVM
+//
+//  Created by 郭伟林 on 16/12/5.
+//  Copyright © 2016年 SR. All rights reserved.
+//
+
+#import "SimpleTableViewCell.h"
+
+@interface SimpleTableViewCell ()
+
+@end
+
+@implementation SimpleTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        
+    }
+    return self;
+}
+
+- (void)setSimpleModel:(SimpleModel *)simpleModel {
+    
+    _simpleModel = simpleModel;
+    
+    self.textLabel.text = simpleModel.text;
+}
+
+@end
